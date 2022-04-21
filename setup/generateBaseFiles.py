f47b9dc39f7c9c45cb2599117187525677966dec
ali <mdali2016.227@gmail.com>
2022-04-21 12:34:04 +0600

    [Change] setup files

--- setup/generateBaseFiles.py
+++ setup/generateBaseFiles.py
@@ -2,6 +2,7 @@
 
 import os
 import shutil
+import subprocess
 
 directories = (
     'app/Helpers',
@@ -15,7 +16,6 @@ directories = (
     'app/Http/Services/Feature',
     'app/Http/Services/Feature/Auth'
 )
-
 for dir in directories:
     if not os.path.exists(dir):
         os.mkdir(dir)
@@ -33,10 +33,19 @@ files = (
     '/Http/Services/Base/UserService',
     '/Http/Services/Feature/Auth/SocialAuthService'
 )
-
 for file in files:
     if not os.path.exists('app'+file+'.php'):
         src_file = open('setup'+file+'.txt', 'r')
         f = open('app'+file+'.php', 'w+')
         f.write(src_file.read())
         f.close()
+
+subprocess.run(["composer","require","laravel/socialite"])
+print('Laravel Socialite Installed')
+subprocess.run(["composer","require","laravel/passport"])
+print('Laravel Passport Installed')
+subprocess.run(["php","artisan","migrate"])
+print('Migration Completed')
+subprocess.run(["php","artisan","passport:install"])
+print('Laravel Passport HasApiToken Added to User.php')
+subprocess.run(["php","artisan","passport:keys"])
