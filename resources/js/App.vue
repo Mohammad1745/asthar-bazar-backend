e77c5529839a73ac6923abde0524ce15fe335933
ali <mdali2016.227@gmail.com>
2022-04-23 11:04:06 +0600

    [Add] Vue in Laravel

---
+++ resources/js/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div>
+            <transition name="fade">
+                <router-view></router-view>
+            </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App.vue"
+}
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s
+}
+.fade-enter, .fade-leave-active {
+    opacity: 0
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
-require('./bootstrap');
+
+// require('./bootstrap');
+//
+// window.Vue = require('vue').default;
+//
+//
+// // const files = require.context('./', true, /\.vue$/i)
+// // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+//
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+//
+// const app = new Vue({
+//     el: '#app',
+// });
+
+import Vue from "vue";
+
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+import VueAxios from "vue-axios";
+import axios from "axios";
+Vue.use(VueAxios, axios);
+
+import App from './App.vue'
+import {routes} from "./router";
+const router = new VueRouter({mode: 'history', routes});
+new Vue(Vue.util.extend({router}, App)).$mount('#app')
