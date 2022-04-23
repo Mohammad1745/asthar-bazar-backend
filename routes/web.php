f980a433cb81648b21b10fe6e80cf32703c2e02a
ali <mdali2016.227@gmail.com>
2022-04-23 12:21:45 +0600

    [Add] default web route

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('default');
+Route::fallback( function () {
+    return redirect(route('default'));
 });
